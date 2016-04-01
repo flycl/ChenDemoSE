926c5f5a3ee3d0805e5700726b31e50581a3c91e
Chen OS <flycl@Chen-OS.local>
2016-04-01 14:30:22 +0800

    第一版本1.0

---
+++ ChenDemo/ChenDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChenDemo
+//
+//  Created by Chen OS on 16/4/1.
+//  Copyright © 2016年 Chen OS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChenDemo/ChenDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChenDemo
+//
+//  Created by Chen OS on 16/4/1.
+//  Copyright © 2016年 Chen OS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
